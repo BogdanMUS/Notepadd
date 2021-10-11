bfbbdbc9735e6beba2776f721ed065a3c4146259
Богдан Мысник <musnik1@outlook.com>
2021-10-11 20:52:58 +0500

    переход на 2 форму

--- Notpad/Notpad/Form1.Designer.cs
+++ Notpad/Notpad/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace Notpad
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -63,12 +64,23 @@ namespace Notpad
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 82);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "на вторая форма";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.ClientSize = new System.Drawing.Size(1193, 341);
+            this.ClientSize = new System.Drawing.Size(410, 341);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
@@ -84,6 +96,7 @@ namespace Notpad
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
